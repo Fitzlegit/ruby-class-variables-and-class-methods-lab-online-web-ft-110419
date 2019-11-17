c507abef536638002e4a62515d4b7f00e2b4eb49
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-11-17 16:16:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,21 @@ class Song
   
   attr_accessor :name, :artist, :genre
   
+  @@count = 0
+  @@genre = []
+  
   def initalize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
+    @@genre << @genre
   end
   
+  def count 
+    @@count
+  end
   
+  def genre
+    @@genre
 end
